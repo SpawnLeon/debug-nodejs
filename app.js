f2e681e8a9d1d0726313e23268edae420b0dbd67
Alexander Leonov <spawnleon@gmail.com>
2021-05-23 17:03:56 +0300

    refactor: add  linters, airbnb config and format code

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
+
 const app = express();
 const db = require('./db');
 const user = require('./controllers/usercontroller');
-const game = require('./controllers/gamecontroller')
+const game = require('./controllers/gamecontroller');
+
 const PORT = 4000;
 
 db.sync();
 app.use(express.json());
 app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'))
+app.use(require('./middleware/validate-session'));
+
 app.use('/api/game', game);
-app.listen(PORT, function() {
-    console.log(`App is listening on ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`App is listening on ${PORT}`);
+});
--- db.js
+++ db.js
@@ -1,19 +1,19 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-//database username   password
+// database username   password
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres'
-})
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+});
 
 sequelize.authenticate().then(
-    function success() {
-        console.log("Connected to DB");
-    },
+  () => {
+    console.log('Connected to DB');
+  },
 
-    function fail(err) {
-        console.log(`Error: ${err}`);
-    }
-)
+  (err) => {
+    console.log(`Error: ${err}`);
+  },
+);
 
 module.exports = sequelize;
